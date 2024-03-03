3482a4cd0cdffae65118ad51df67893122854001
Dimativ <Dimativtoptinker@gmail.com>
2024-03-03 22:05:28 +0200

    Modal component feat: added zustand state manager feat: added modal provider feat: added hooks for modal state

---
+++ components/ui/modal.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from './dialog';
+
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: ReactNode;
+}
+export const Modal: FC<ModalProps> = ({ title, onClose, isOpen, description, children }) => {
+  const onChange = (open: boolean) => {
+    if (!isOpen) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
