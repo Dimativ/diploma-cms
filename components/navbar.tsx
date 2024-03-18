0a5013bee96841b3afba90a0a3f0baae933d03c0
Dimativ <88792228+Dimativ@users.noreply.github.com>
2024-03-18 02:25:01 +0200

    Implemented billboards functionality (#8)
    feat: implemented billboards page
    feat: added api routes for billboards
    feat: added model and relations for billboards
    feat: added data table with custom filtering
    feat: added api-list component
    feat: implemented image download component using cloudinary
    fix: issue with cors while using cloudinary

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = async () => {
 
   return (
     <div className="border-b">
-      <div className="flex items-center p-4">
+      <div className="flex items-center px-8 py-4">
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
